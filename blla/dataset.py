5ff3b77807087b941fa79d3619c3bee42a979fc7
Benjamin Kiessling <mittagessen@l.unchti.me>
2019-04-16 17:33:43 +0200

    new datasets with precomputed features

--- blla/dataset.py
+++ blla/dataset.py
@@ -1,3 +1,4 @@
+import json
 import torch
 import numpy as np
 import torchvision.transforms.functional as tf
@@ -6,7 +7,9 @@ from PIL import Image
 from torch.utils import data
 from torchvision import transforms
 
-class BaselineSet(data.Dataset):
+from scipy.ndimage import morphology
+
+class InitialVertexDataset(data.Dataset):
 
     def __init__(self, imgs):
         super().__init__()
@@ -14,20 +17,54 @@ class BaselineSet(data.Dataset):
 
     def __getitem__(self, idx):
         im = self.imgs[idx]
-        target = Image.open('{}.seeds.png'.format(im))
-        orig = Image.open('{}.plain.png'.format(im))
-        return self.transform(orig, target)
+        with open('{}.lines.json'.format(im)) as fp:
+            target = json.load(fp)
+        feats = torch.load('{}.feat'.format(im))
+        return self.transform(feats, target)
 
-    def transform(self, image, target):
-        resize = transforms.Resize(900)
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
+    def transform(self, feats, targets):
+        vertices = [min(t, key=lambda x: x[0]) for t in target['lines']]
+        scale_x = feats.shape[3] / target['orig'][0]
+        scale_y = feats.shape[2] / target['orig'][1]
+        tim = np.zeros(image.size)
+        for v in vertices:
+            tim[int(v[0]*scale_x), int(v[1]*scale_y)] = 255
+        tim = morphology.binary_dilation(tim, iterations=2)*255
+        target = Image.fromarray(tim.T.astype('uint8'))
+        return feats, tf.to_tensor(target)
 
-        image = resize(image)
-        target = resize(target)
+    def __len__(self):
+        return len(self.imgs)
 
-        target = Image.fromarray(((np.array(target) > 0) * 255).astype('uint8'))
-        return normalize(tf.to_tensor(image.convert('RGB'))), tf.to_tensor(target)
+class VerticesDataset(data.Dataset):
+
+    def __init__(self, imgs):
+        super().__init__()
+        self.imgs = [x[:-10] for x in imgs]
+        self.len = 0
+        for im in self.imgs:
+            with open('{}.lines.json'.format(im)) as fp:
+                self.len += len(json.load(fp))
+
+    def __getitem__(self, idx):
+        im = self.imgs[idx]
+        with open('{}.lines.json'.format(im)) as fp:
+            target = json.load(fp)
+        feats = torch.load('{}.feat'.format(im))
+        # create time series of targets
+        l = np.random.choice(target['lines'])
+        # TODO: scale targets
+        if l[0][0] > l[-1][0]:
+            l = list(reversed(l))
+        points = [line(*start, *end) for start, end in zip(l, l[1::])]
+        x = np.hstack([x[0] for x in points])[::10]
+        y = np.hstack([x[1] for x in points])[::10]
+        targets = []
+        for target in np.array((x, y)).T:
+            t = np.zeros(target['orig'])
+            t[target.T] = 1
+            targets.append(t)
+        return feats, torch.tensor(np.stack(targets))
 
     def __len__(self):
         return len(self.imgs)
