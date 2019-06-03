a79e751080bc20b68b96e15c30eb3be45ca37ae4
Benjamin Kiessling <mittagessen@l.unchti.me>
2019-06-03 23:22:26 +0200

    Change training data format
    The old one required great care in producing the masks to ensure the
    baselines are roughly equal in height after scaling. The new one is just
    a bunch of json files with a list of lines

--- blla/dataset.py
+++ blla/dataset.py
@@ -1,8 +1,9 @@
+import json
 import torch
 import numpy as np
 import torchvision.transforms.functional as tf
 
-from PIL import Image
+from PIL import Image, ImageDraw
 from torch.utils import data
 from torchvision import transforms
 from scipy.ndimage import gaussian_filter
@@ -16,18 +17,28 @@ class BaselineSet(data.Dataset):
 
     def __getitem__(self, idx):
         im = self.imgs[idx]
-        target = Image.open('{}.seeds.png'.format(im))
+        with open('{}.path'.format(im), 'r') as fp:
+            target = json.load(fp)
         orig = Image.open('{}.plain.png'.format(im))
         return self.transform(orig, target)
 
     def transform(self, image, target):
-        resize = transforms.Resize(900)
+        resize = transforms.Resize(1200)
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
-
+        orig_size = image.size
         image = resize(image)
-        target = resize(target)
-        target = ((np.array(target) > 0) * 255).astype('uint8')
+        scale = image.size[0]/orig_size[0]
+        t = Image.new('L', image.size)
+        draw = ImageDraw.Draw(t)
+        lines = []
+        for line in target:
+            l = []
+            for point in line:
+                l.append((int(point[0]*scale), int(point[1]*scale)))
+            draw.line(l, fill=255, width=10)
+        del draw
+        target = np.array(t)
         if self.smooth:
             target = gaussian_filter(target, sigma=2)
         target = Image.fromarray(target)
